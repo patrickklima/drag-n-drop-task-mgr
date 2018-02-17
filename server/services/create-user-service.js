d925c04fc4b37ff5a763420d73b4983dde0e2bc3
unknown <patrick.klima@gmail.com>
2018-02-17 15:35:48 -0600

    Bug Fixes to /register route
    Updates /register route, now tested with Postman.
    
    Server runs and accepts registrations successfully.

--- server/services/create-user-service.js
+++ server/services/create-user-service.js
@@ -3,12 +3,18 @@ const {User} = require('../data/models');
 
 module.exports = (app) => {
   app.post('/register', (req, res, next) => {
-    User.register(
-      new User({username : req.body.username}), req.body.password
-    ).then((user) => {
-      passport.authenticate('local');
-      return user;
-    }).then((user) => res.json(JSON.stringify(user))
-    ).catch(err => next(err));
+    User.register(new User(
+      {username : req.body.username}), 
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          return next(err);
+        }
+        passport.authenticate('local');
+        res.json(JSON.stringify(user));
+      }
+    )
   });
+
+
 }
