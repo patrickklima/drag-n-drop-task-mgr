7f51d79963d059d584faebb5bb6f8700bad19eca
Patrick Klima <patrick.klima@gmail.com>
2018-03-29 15:41:06 -0500

    Adds userId param to createBoard.
    Refactors to allow create-data methods to call each other.

--- server/services/create-data-service.js
+++ server/services/create-data-service.js
@@ -2,27 +2,35 @@ const models = require('../data/models');
 
 const errorCatch = (err) => console.log("error in create-data-service", err);
 
-module.exports = {
-  createCard: async (cardTitle, listId) => {
-    console.log("createcard", cardTitle, listId);
-    const newCard = await new models.Card({cardTitle}).save().catch(errorCatch);
-    let parentList = await models.List.findById(listId).catch(errorCatch);
-    parentList.cards = [...parentList.cards, newCard._id];
-    return await parentList.save().catch(errorCatch);
-  },
-  createList: async (listTitle, boardId) => {
-    console.log("createList", listTitle, boardId);
-    const newList = await new models.List({listTitle}).save().catch(errorCatch);
-    let parentBoard = await models.Board.findById(boardId).catch(errorCatch);
-    parentBoard.lists = [...parentBoard.lists, newList._id];
-    return await parentBoard.save().catch(errorCatch);
-  },
-  createBoard: async (boardTitle) => {
-    console.log("createBoard", boardTitle);
-    const newBoard = await new models.Board({boardTitle}).save().catch(errorCatch);
-    const newList = await this.createList("New List", newBoard._id);
-    const newCard = await this.createCard("New Card", newlist._id);
-    return newBoard;
-  },
+let service = {};
+service.createCard = async (cardTitle, listId) => {
+  console.log("createcard", cardTitle, listId);
+  const newCard = await new models.Card({cardTitle}).save().catch(errorCatch);
+  let parentList = await models.List.findById(listId).catch(errorCatch);
+  parentList.cards = [...parentList.cards, newCard._id];
+  await parentList.save().catch(errorCatch);
+  return newCard;
+};
+service.createList = async (listTitle, boardId) => {
+  console.log("createList", listTitle, boardId);
+  const newList = await new models.List({listTitle}).save().catch(errorCatch);
+  let parentBoard = await models.Board.findById(boardId).catch(errorCatch);
+  parentBoard.lists = [...parentBoard.lists, newList._id];
+  await parentBoard.save().catch(errorCatch);
+  return newList;
+};
+service.createBoard = async (boardTitle, userId) => {
+  console.log("createBoard", boardTitle, userId);
+  const newBoard = await new models.Board({
+    boardTitle,
+    lists: [],
+    members: [userId]
+  }).save().catch(errorCatch);
+  console.log("createBoard is done", newBoard);
+  const newList = await service.createList("New List", newBoard._id);
+  const newCard = await service.createCard("New Card", newList._id);
+  console.log("newBoard", newBoard);
+  return newBoard;
 };
 
+module.exports = service;
