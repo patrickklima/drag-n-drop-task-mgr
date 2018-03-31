7ab3395379bd142e885270b1a40ea17cfb0a72b7
Patrick Klima <patrick.klima@gmail.com>
2018-03-30 19:54:18 -0500

    Adds tokenization for authenticated users.
    Adds password comparison against hashed passwords in the db.

--- server/routes/login-route.js
+++ server/routes/login-route.js
@@ -1,21 +1,28 @@
 const passport = require('passport');
 const {getFullUserById, getFullUserByUserName} = require('../services/get-data-service');
-
+const {makeToken} = require('../services/jwt-service');
+const {comparePasswords} = require('../services/password-service');
 
 module.exports = (app) => {
-  app.post('/login', 
-    passport.authenticate('local'), 
-    (req, res, next) => {
-      getFullUserByUserName(req.body.username)
-      .then(user => {
-        req.user.boards = user[0].boards;
-        req.user.cards = user[0].cards;
-        return res.json(user);
-      })
-      .catch(err => next(err));
-    }
-  )
-  
+  app.post('/login', (req, res, next) => {
+    return getFullUserByUserName(req.body.username)
+    .then(userData => {
+      let user = userData[0];
+      const isAuthorizedUser = comparePasswords(req.body.password, user.password);
+      if (isAuthorizedUser) {
+        const token = makeToken({id: user._id})
+        return res.json([{user, token}]);
+      } else {
+        let err = new Error("Invalid password. User login failed.")
+        err.response = {
+          status: 401
+        };
+        throw err;
+      }
+    })
+    .catch(err => next(err));
+  });
+    
   app.get('/authenticate', 
     passport.authenticate('local'),
     (req, res, next) => res.sendStatus(200));
