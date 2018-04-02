30169506aa34cffbcd118a785d8cfed8fbd8034d
Patrick Klima <patrick.klima@gmail.com>
2018-04-01 20:18:29 -0500

    Adds List model methods for adding and removing cards.

--- server/data/models/list.js
+++ server/data/models/list.js
@@ -11,11 +11,31 @@ var ListSchema = new Schema({
 
 ListSchema.statics.getListAndCards = (id) => {
   return List.find({_id: id}).populate('cards');
-}
+};
 
 ListSchema.statics.updateOne = (id, data) => {
   return List.findByIdAndUpdate(id, data);
-}
+};
+ListSchema.statics.addCard = (id, newCardId) => {
+  console.log("addCard", id, newCardId);
+  return List.findById(id)
+  .then(list => {
+    list.cards = [...list.cards, newCardId];
+    return list.save();
+  });
+};
+ListSchema.statics.removeCard = (id, removedCardId) => {
+  console.log("removeCard", id, removedCardId);
+  return List.findById(id)
+  .then(list => {
+    list.cards = list.cards.filter(thisCardId => {
+      console.log("thisCardId", thisCardId, "removedCardId", removedCardId, thisCardId != removedCardId); 
+      return thisCardId != removedCardId;
+    });
+    console.log("List after filter", list.listTitle, list.cards);
+    return list.save();
+  });
+};
 
 
 var List = mongoose.model('List', ListSchema);
